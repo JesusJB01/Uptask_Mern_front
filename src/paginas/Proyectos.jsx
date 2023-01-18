96752df701acafc96aba9ff0626df4ab8de6a9f8
JesusJB01 <abelbracho@40gamil.com>
2023-01-17 20:45:03 -0500

    my first  commit

---
+++ src/paginas/Proyectos.jsx
@@ -0,0 +1,31 @@
+import Alerta from "../components/Alertas";
+import PreviewProyecto from "../components/PreviewProyecto";
+import useProyectos from "../hooks/useProyectos";
+
+
+
+
+const Proyectos = () => {
+  const { proyectos, alerta } = useProyectos();
+
+
+  const { msg } = alerta;
+
+  return (
+    <>
+      <h1 className="text-4xl font-black">Proyectos</h1>
+      {msg && <Alerta alerta={alerta} />}
+      <div className="bg-white shadow mt-10 rounded-lg ">
+        {proyectos.length ? (
+          proyectos.map((proyecto) => (
+            <PreviewProyecto key={proyecto._id} proyecto={proyecto} />
+          ))
+        ) : (
+          <p className="text-center text-gray-600 uppercase p-5">no hay</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Proyectos;
